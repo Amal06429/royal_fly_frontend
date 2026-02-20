c1f3d614e5087a5939f87d09791d88d929d85414
Amal <amal06429@gmail.com>
2026-02-20 15:25:34 +0530

    feat: Add CaseSensitivePaths plugin to Vite configuration for cross-platform compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import CaseSensitivePaths from "vite-plugin-case-sensitive-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    CaseSensitivePaths(), // ✅ handles Windows → Linux mismatch
+  ],
+});
